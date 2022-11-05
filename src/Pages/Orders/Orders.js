b2253a832bafdabe0b2ec8fcc518052bd38a09bc
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-06 03:22:19 +0600

    set conditional pending or aprove button and delete and display data

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -9,7 +9,7 @@ const Orders = () => {
 
     useEffect(() => {
         // console.log('user', user);
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
+        fetch(`http://localhost:5000/orders?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setOrders(data))
     }, [user?.email])
@@ -25,11 +25,33 @@ const Orders = () => {
                     console.log(data);
                     if (data.deletedCount > 0) {
                         alert('deleted successfully');
-                        const remaining = orders.filter(odr => orders._id !== id);
+                        const remaining = orders.filter(odr => odr._id !== id);
                         setOrders(remaining);
                     }
                 })
         }
+    }
+
+    const handleStatusUpdate = id => {
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'Approved' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    const remaining = orders.filter(odr => odr._id !== id);
+                    const approving = orders.find(odr => odr._id === id);
+                    approving.status = 'Approved'
+
+                    const newOrders = [approving, ...remaining];
+                    setOrders(newOrders);
+                }
+            })
     }
 
     return (
@@ -40,9 +62,6 @@ const Orders = () => {
                     <thead>
                         <tr>
                             <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
                             </th>
                             <th>Name</th>
                             <th>Job</th>
@@ -57,6 +76,7 @@ const Orders = () => {
                                 key={order._id}
                                 order={order}
                                 handleDelete={handleDelete}
+                                handleStatusUpdate={handleStatusUpdate}
                             ></OrderRow>)
                         }
                     </tbody>
